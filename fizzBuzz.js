f6828f304227afeb842963098bfab8fa0b9dda31
Aiden Kampman <aidenshanekampman@gmail.com>
2022-02-10 10:04:29 -0700

    Demonstration complete

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,10 @@
 // Write your solution below this line:
+for (x = 1; x <= 50; x++){
+    if (x%3 === 0 && x%5 === 0){
+        console.log('FizzBuzz')
+    }else if (x%3 === 0){
+            console.log('Fizz')
+        }else if (x%5 === 0){
+            console.log('Buzz')
+        }else console.log(x)
+}
